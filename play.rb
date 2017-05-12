f08d24e0235da22d335e6d9e567d57af348e40eb
Michael Newton <mn@one-timepad.com>
2017-05-12 17:33:14 -0400

    Change title

--- play.rb
+++ play.rb
@@ -6,7 +6,7 @@ game = nil
 
 loop do
   system('clear')
-  puts "RubyChess!!!".colorize(:yellow)
+  puts "BvY Chess!!!".colorize(:yellow)
   puts "Play vs. computer or human?".colorize(:red)
   puts "Enter 1 for computer, 2 for human.".colorize(:blue)
   str = gets.chomp
